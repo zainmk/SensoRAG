042d9b0faf3676b970d63adee2d82ff34ea88536
zainmk <zain.mk@hotmail.com>
2026-05-16 13:23:25 -0600

    simplify

--- server.py
+++ server.py
@@ -17,11 +17,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/test')
-def test():
-    return 'hello world'
-
-
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
