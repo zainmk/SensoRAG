63eb16b3b51fb3d3074559080b040c496460450b
zainmk <zain.mk@hotmail.com>
2026-05-16 12:30:43 -0600

    render config

--- server.py
+++ server.py
@@ -55,4 +55,5 @@ def search():
 
 if __name__ == '__main__':
     print("[sensoRAG] Starting server at http://localhost:5000")
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
